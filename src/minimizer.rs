fe619af9c45424c4fbba1d52939f44bb16bf8536
Igor Martayan <igor@martayan.org>
2024-09-12 13:54:29 +0200

    Fix test_sliding_min (thanks Ragnar)

--- src/minimizer.rs
+++ src/minimizer.rs
@@ -75,19 +75,14 @@ mod tests {
         let w = 3;
         let hashes = [7, 5, 9, 8, 6, 3].map(|x| x << 16);
         let hashes: [u32; 6 * LANES] = from_fn(|i| hashes[i / LANES]);
-        let expected = [5, 5, 6, 3];
-        let expected: [u32; 4 * LANES] = from_fn(|i| expected[i / LANES]);
+        let expected = [1, 1, 4, 5]; // positions of the minimizers
+        let expected: [u32; 4 * LANES] = from_fn(|i| expected[i / LANES] + 4 * (i % LANES) as u32);
 
         let chunks = hashes.as_chunks::<8>().0;
-        // dbg!(chunks);
-        let it = chunks.iter().map(|&t| S::new(t));
-        // dbg!(it.size_hint());
+        let it = chunks.iter().map(|&t| t.into());
 
-        // for mini in sliding_min_par_it(it, w) {
-        //     dbg!(mini.to_array().map(|x| x >> 16));
-        // }
         let res: Vec<u32> = sliding_min_par_it(it, w)
-            .flat_map(|t| t.to_array().map(|x| x >> 16))
+            .flat_map(|t| t.to_array())
             .collect();
         assert_eq!(res, expected);
     }
