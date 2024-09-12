41d75613817ae2ca5c490f677f9d177afae4891b
Igor Martayan <igor@martayan.org>
2024-09-12 12:06:38 +0200

    Init cargo

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+#![feature(slice_as_chunks)]
+
+pub mod minimizer;
+pub mod ringbuf;
+
+use biotest::Format;
+
+pub fn gen_seq(len: usize) -> Vec<u8> {
+    let mut rng = biotest::rand();
+    let mut seq = Vec::with_capacity(len);
+    let generator = biotest::Sequence::builder()
+        .sequence_len(len)
+        .build()
+        .unwrap();
+    generator.record(&mut seq, &mut rng).unwrap();
+    seq
+}
