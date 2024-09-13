47636cc2ac4cbb51d2da6886c0d4a8eee3188958
Igor Martayan <igor@martayan.org>
2024-09-13 12:19:14 +0200

    Split gather implementation for AVX

--- src/intrinsics/gather.rs
+++ src/intrinsics/gather.rs
@@ -6,14 +6,22 @@ use wide::u64x4;
 //     unsafe { Simd::<u64, 2>::gather_ptr(source) }
 // }
 
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[target_feature(enable = "avx")]
+unsafe fn gather_avx(source: u64x4) -> u64x4 {
+    #[cfg(target_arch = "x86")]
+    use std::arch::x86::*;
+    #[cfg(target_arch = "x86_64")]
+    use std::arch::x86_64::*;
+
+    unimplemented!()
+}
+
 #[inline(always)]
 pub fn gather(source: u64x4) -> u64x4 {
-    #[cfg(all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        target_feature = "avx"
-    ))]
-    {
-        unimplemented!()
+    #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+    if std::is_x86_feature_detected!("avx") {
+        return unsafe { gather_avx(a, b) };
     }
     unsafe { u64x4::new(source.to_array().map(|p| *(p as *const u64))) }
 }
