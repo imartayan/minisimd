9a429acb7a0d82f23561ab23067831a9325a23fb
Igor Martayan <igor@martayan.org>
2024-09-16 16:07:04 +0200

    Implement gather for AVX512

--- src/intrinsics/gather.rs
+++ src/intrinsics/gather.rs
@@ -7,21 +7,24 @@ use wide::u64x4;
 // }
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-#[target_feature(enable = "avx")]
-unsafe fn gather_avx(source: u64x4) -> u64x4 {
+#[target_feature(enable = "avx512f")]
+unsafe fn gather_avx(ptr: *const u8, offsets: u64x4) -> u64x4 {
     #[cfg(target_arch = "x86")]
-    use std::arch::x86::*;
+    use std::arch::x86::_mm512_i64gather_epi64;
     #[cfg(target_arch = "x86_64")]
-    use std::arch::x86_64::*;
+    use std::arch::x86_64::_mm512_i64gather_epi64;
 
-    unimplemented!()
+    _mm512_i64gather_epi64(offsets, ptr, 1)
 }
 
 #[inline(always)]
-pub fn gather(source: u64x4) -> u64x4 {
+pub fn gather(ptr: *const u8, offsets: u64x4) -> u64x4 {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-    if std::is_x86_feature_detected!("avx") {
+    if std::is_x86_feature_detected!("avx512f") {
         return unsafe { gather_avx(source) };
     }
-    unsafe { u64x4::new(source.to_array().map(|p| *(p as *const u64))) }
+    unsafe {
+        let source = u64x4::splat(ptr as u64) + offsets;
+        u64x4::new(source.to_array().map(|p| *(p as *const u64)))
+    }
 }
